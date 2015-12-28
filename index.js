272e9c2e6e654707ab9ecdb944629f1d8d6794c7
iammyth <iammyth@foxmail.com>
2015-12-28 17:23:26 +0800

    make it better

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    app = express(),
+    config = {
+        token: 'token',
+        appid: 'wx915838469a97a026',
+        encodingAESKey: 'd4624c36b6795d1d99dcf0547af5443d'
+    };
+
+app.use(express.query());
+app.use('/wechat', wechat(config, function(req, res, next) {
+    // 微信输入信息都在req.weixin上
+    var message = req.weixin;
+    res.reply("You typed: "+ message.content);
+}));
+
+app.get('/', function(req, res) {
+    res.send('Hello World')
+});
+
+app.listen(process.env.PORT || 5050);
